e4f065f09a2a2746649fbd454f79948a6717ebe5
Andy <andrew.honey@southeasttech.edu>
2026-02-06 10:25:26 -0600

    fixed routing issues with images

--- src/pages/Discord.tsx
+++ src/pages/Discord.tsx
@@ -1,10 +1,11 @@
+import qrcode from '/src/images/qrcode.png';
 const Discord = () => 
 <div>
 
     <h2>Join the Discord and get started!</h2>
-    <img src="/qrcode.png"></img><br></br>
+    <img src={qrcode}></img><br></br>
     <a href="https://discord.gg/ARX38fdVBc">Click here to join</a>
-
+    <script>console.log()</script>
 </div>;
 
 
