f8f070b9ae7ad1426462571301053995be77ba72
Andy <andrew.honey@southeasttech.edu>
2026-02-09 12:58:54 -0600

    Updated Game Jam Event

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -2,7 +2,8 @@ const Events = () =>
 <div>
     <h1>Game Jam</h1>
     <h2>Monday, May 11th 2026 – Friday May 15th 2026</h2>
-    Drop into the <a href='/discord'>Discord</a> and join us for the theme announcement!
+    Drop into the <a href="https://discord.gg/ARX38fdVBc">Discord</a> and join us for the theme announcement!<br></br>
+    <a href={`${import.meta.env.BASE_URL}stcGameJam.ics`} target="_blank">Need a calendar reminder?</a>
 </div>;
 
 export default Events;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,6 @@
 const Projects = () => 
 <div>
-    <p>No projects yet. <a href="/discord">Join our Discord</a> to change that!</p>
+    <p>No projects yet. Join our Discord to change that!</p>
 </div>;
 
 export default Projects;
